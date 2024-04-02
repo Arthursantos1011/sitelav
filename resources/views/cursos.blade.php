20df8c27eac759efa791d310d660166531e63a4f
Arthursantos1011 <arthur.santos210@etec.sp.gov.br>
2024-04-02 08:55:00 -0300

    Arthur

---
+++ resources/views/cursos.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Innovation Pro Systems</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<link href="img/icone1.ico" rel="sortcut icon"/>
+		<meta name ="viewport" content ="width=devide-width,heigth=devide-height,initial-scale=1.0"/>
+	</head>
+	<body>
+		<nav>
+			<img src="img/innovation2.png"/>
+			<ul>
+            <li><a href="">HOME</a></li>
+				<li><a href="/">DEPARTAMENTOS</a></li>
+				<li><a href="/"target="_blank">OPORTUNIDADES</a></li>
+				<li><a href="/">VESTIBULINHO</a></li>
+                <li><a href="/">INSTITUIÇÃO</a></li>
+                <li><a href="/">CONTATO</a></li>
+				<li><a href="/"><img src="img/brasil.png"/></a></li>
+			</ul>
+		</nav>
+		<br/>
+		<p class="texto"><b>SERVIÇOS</b></p>
+		<hr/>
+		<br/><br/>
+		<section id="segmentos">
+		<div>
+             <img src="img/web_development.png"/>
+             <h3>Desenvolvimento de software</h3>
+             <P>Assim como cada cliente é único, nossos produtos também são. Criamos o site exclusivamente para o seu projeto.</P>
+        </div>
+        <div>
+             <img src="img/banco-de-dados.png"/>
+             <h3>Administração</h3>
+             <P>Nosso sistema de gestão conta com uma equipe capacitada para os serviços de consultoria especializada em projetos, implementações e administração em banco de dados.</P>
+        </div>
+        <div>
+             <img src="img/codigo.png"/>
+             <h3>Recursos Humanos</h3>
+             <P>Desenvolvimento de sistemas totalmente personalizados para sua empresa e suas necessidades.</P>
+        </div>
+        <div>
+             <img src="img/ideia.png"/>
+             <h3>Logística</h3>
+             <P>Com toda a nossa criatividade e a técnica ajudamos desenvolver interfaces digitais interativas, atrativas e eficazes.</P>
+        </div>
+		</section>
+		<br/><br/><br/><br/>
+		<footer>
+			<P>Copyright© 2020 Innovation Pro Systems&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.instagram.com/innovationprosystems/?hl=pt-br" target="_blank"><img src="img/instagram1.png"/></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/Innovation-Pro-Systems-108111967753711/" target="_blank"><img src="img/facebook1.png"/></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/innovation-pro-systems-829a161b9/" target="_blank"><img src="img/linkedin1.png"/><a/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.google.com/maps/place/R.+Guaip%C3%A1,+678+-+Vila+Leopoldina,+S%C3%A3o+Paulo+-+SP,+02675-031/@-23.520918,-46.728762,16z/data=!4m5!3m4!1s0x94cef8c1d3806777:0x8d03885c01c45d8a!8m2!3d-23.5209181!4d-46.728762?hl=pt-BR" target="_blank"><img src="img/pin.png"/><a/></P>
+		</footer>
+	</body>
+</html>
